357f0f43bb2358e87f98aa59dda9eeeb3d61901a
Sebastian <sebastianalmagro2001@gmail.com>
2024-03-18 22:31:46 -0300

    Corrigiendo mi entrega

--- productManager.js
+++ productManager.js
@@ -1,55 +1,56 @@
 class ProductManager {
-    constructor() {
-      this.products = [];
-    }
-  
-    addProduct(product) {
-      this.products.push(product);
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(productId) {
-      const product = this.products.find((p) => p.code === productId);
-  
-      if (product) {
-        return product;
-      } else {
-        console.error('Not found');
-        return null;
-      }
-    }
+  constructor() {
+    this.products = [];
+    this.nextId = 1;
   }
 
-  const manager = new ProductManager();
-  
-  const product1 = {
-    title: 'AMD Radeon RX 580',
-    description: 'Tarjeta grafica AMD Radeon RX 580',
-    price: 550000,
-    thumbnail: 'GamingHouse/Graficas/AMDRadeonRX580.jpg',
-    code: 'P001',
-    stock: 7,
-  };
-  
-  const product2 = {
-    title: 'Geforce Rtx 3060 8gb',
-    description: 'Tarjeta grafica Geforce Rtx 3060 8gb',
-    price: 39.99,
-    thumbnail: 'GamingHouse/Graficas/GeforceRtx30608gb.jpg',
-    code: 'P002',
-    stock: 9,
-  };
-  
-  manager.addProduct(product1);
-  manager.addProduct(product2);
-  
-  console.log(manager.getProducts());
-  
-  const foundProduct = manager.getProductById('P001');
-  console.log(foundProduct);
-  
-  const notFoundProduct = manager.getProductById('P003');
+  addProduct(product) {
+    product.code = 'P' + this.nextId++;
+    this.products.push(product);
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(productId) {
+    const product = this.products.find((p) => p.code === productId);
+
+    if (product) {
+      return product;
+    } else {
+      console.error('Not found');
+      return null;
+    }
+  }
+}
+
+const manager = new ProductManager();
+
+const product1 = {
+  title: 'AMD Radeon RX 580',
+  description: 'Tarjeta grafica AMD Radeon RX 580',
+  price: 550000,
+  thumbnail: 'GamingHouse/Graficas/AMDRadeonRX580.jpg',
+  stock: 7,
+};
+
+const product2 = {
+  title: 'Geforce Rtx 3060 8gb',
+  description: 'Tarjeta grafica Geforce Rtx 3060 8gb',
+  price: 39.99,
+  thumbnail: 'GamingHouse/Graficas/GeforceRtx30608gb.jpg',
+  stock: 9,
+};
+
+manager.addProduct(product1);
+manager.addProduct(product2);
+
+console.log(manager.getProducts());
+
+const foundProduct = manager.getProductById('P1');
+console.log(foundProduct);
+
+const notFoundProduct = manager.getProductById('P3');
+
   
